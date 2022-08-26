27af529856a0c637af0e53087b76daf52e748596
kaponda <stefkap11@gmail.com>
2022-08-26 06:40:41 +0200

    fin entité locality

---
+++ src/Controller/LocalityController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Locality;
+class LocalityController extends AbstractController
+{
+    /**
+     * @Route("/locality", name="app_locality")
+     */
+    public function index(): Response
+    {
+       $repository = $this->getDoctrine()->getRepository(Locality::class);
+        $localities = $repository->findAll();
+
+        return $this->render('locality/index.html.twig', [
+            'localities' => $localities,
+            'resource' => 'localities',
+
+        ]);
+
+    }
+	    /**
+     * @Route("/locality/{id}", name="locality_show")
+     */
+    public function show($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Locality::class);
+       $localitie = $repository->find($id);
+
+        return $this->render('locality/show.html.twig', [
+            'localitie' => $localitie,
+        ]); 
+    }
+
+}
---
+++ src/DataFixtures/LocalityFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Locality;
+class LocalityFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+            $localities = [
+            ['postal_code'=>'1000','locality'=>'Bruxelles'],
+            ['postal_code'=>'1020','locality'=>'Laeken'],
+            ['postal_code'=>'1030','locality'=>'Schaerbeek'],
+            ['postal_code'=>'1050','locality'=>'Ixelles'],
+            ['postal_code'=>'1070','locality'=>'Anderlecht'],
+            ['postal_code'=>'1180','locality'=>'Uccle'],
+        ];
+        
+        foreach ($localities as $record) {
+            $locality = new Locality();
+            $locality->setPostalCode($record['postal_code']);
+            $locality->setLocality($record['locality']);
+            $manager->persist($locality);
+        }
+
+        $manager->flush();
+    }
+}
